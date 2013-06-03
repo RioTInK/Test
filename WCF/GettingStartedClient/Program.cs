91efa2b0be8a8e3a00051da300e0c0699b48ff60
RioT InK <riotink@live.com>
2013-06-03 15:09:22 -0500

    WCF v0.0
    Proyecto de Prueba de WCF.

---
+++ WCF/GettingStartedClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GettingStartedClient.CalculatorServiceReference;
+
+namespace GettingStartedClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create an instance of the WCF proxy.
+            CalculatorClient client = new CalculatorClient();
+
+            double value1 = 100.00D;
+            double value2 = 15.99D;
+
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            double result = client.Add(value1, value2);
+            Console.WriteLine("Add({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Subtract service operation.
+            result = client.Subtract(value1, value2);
+            Console.WriteLine("Subtract({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Multiply service operation.
+            result = client.Multiply(value1, value2);
+            Console.WriteLine("Multiply({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Divide service operation.
+            result = client.Divide(value1, value2);
+            Console.WriteLine("Divide({0},{1}) = {2}", value1, value2, result);
+
+            //Operaciones(client, value1, value2);
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+        }
+    }
+}
